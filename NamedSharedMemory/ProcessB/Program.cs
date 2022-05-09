d0f38f71dd03db0a499a0ae31bfaa42c8ce5cb74
gespriella <gespriella@users.noreply.github.com>
2022-05-09 14:14:04 -0600

    Add files via upload

---
+++ NamedSharedMemory/ProcessB/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Threading;
+
+class Program
+{
+    // Process B:
+    static void Main(string[] args)
+    {
+        try
+        {
+            using (MemoryMappedFile mmf = MemoryMappedFile.OpenExisting("namedsharedmemory"))
+            {
+
+                Mutex mutex = Mutex.OpenExisting("testmapmutex");
+                mutex.WaitOne();
+
+                using (MemoryMappedViewStream stream = mmf.CreateViewStream(1, 0))
+                {
+                    BinaryWriter writer = new BinaryWriter(stream);
+                    writer.Write(false);
+                }
+                mutex.ReleaseMutex();
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("Memory-mapped file does not exist. Run Process A first.");
+        }
+    }
+}
